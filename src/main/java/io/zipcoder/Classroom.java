d6dfc80d36d13fbf41c8ef148554313767b423c7
Jake Lawhorne <jake.lawhorne@gmail.com>
2023-03-17 19:44:26 -0400

    Finished student class testing

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -6,11 +6,10 @@ import java.util.*;
 
 public class Classroom {
 
-    Student student = new Student(null, null, null);
 
     private Student[] students;
 
-    private TreeMap<String, Student[]> gradebook = new TreeMap<>();
+    private TreeMap<String, ArrayList<Student>> gradebook = new TreeMap<>();
 
     public Classroom(){
         students = new Student[30];
@@ -32,7 +31,7 @@ public class Classroom {
     public Double getClassAverage(){
         double sum = 0.0;
         for(int i = 0; i < students.length; i++){
-            sum += student.getAverageScore();
+            sum += students[i].getAverageScore();
         }
         return sum / students.length;
     }
@@ -47,7 +46,7 @@ public class Classroom {
 
     public void removeStudent(String firstName, String lastName){
         for(int i = 0; i < students.length; i++){
-            if(students[i].equals( student.getFirstName().equals(firstName) && student.getLastName().equals(lastName))){
+            if(students[i].equals( students[i].getFirstName().equals(firstName) && students[i].getLastName().equals(lastName))){
                 students[i] = null;
             }
         }
@@ -58,18 +57,30 @@ public class Classroom {
         return students;
     }
 
-    public TreeMap getGradebook(Student... s){
-        if(student.getAverageScore() > (0.90 * getClassAverage())){
-            gradebook.put("A", s);
-        } else if(student.getAverageScore() < (0.90 * getClassAverage()) && student.getAverageScore() > (0.70 * getClassAverage())){
-            gradebook.put("B", s);
-        } else if(student.getAverageScore() < (0.70 * getClassAverage()) && student.getAverageScore() > (0.50 * getClassAverage())){
-            gradebook.put("C", s);
-        } else if(student.getAverageScore() < (0.50 * getClassAverage()) && student.getAverageScore() > 0.10 * getClassAverage()){
-            gradebook.put("D", s);
-        } else if(student.getAverageScore() < (0.10 * getClassAverage())){
-            gradebook.put("F", s);
+    public TreeMap getGradebook() {
+        gradebook.put("A", new ArrayList<>());
+        gradebook.put("B", new ArrayList<>());
+        gradebook.put("C", new ArrayList<>());
+        gradebook.put("D", new ArrayList<>());
+        gradebook.put("F", new ArrayList<>());
+
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].getAverageScore() > (0.90 * getClassAverage())) {
+                gradebook.get("A").add(students[i]);
+            } else if (students[i].getAverageScore() < (0.90 * getClassAverage()) && students[i].getAverageScore() > (0.70 * getClassAverage())) {
+                gradebook.get("B").add(students[i]);
+            } else if (students[i].getAverageScore() < (0.70 * getClassAverage()) && students[i].getAverageScore() > (0.50 * getClassAverage())) {
+                gradebook.get("C").add(students[i]);
+            } else if (students[i].getAverageScore() < (0.50 * getClassAverage()) && students[i].getAverageScore() > 0.10 * getClassAverage()) {
+                gradebook.get("D").add(students[i]);
+            } else if (students[i].getAverageScore() < (0.10 * getClassAverage())) {
+                gradebook.get("F").add(students[i]);
+            }
         }
         return gradebook;
     }
+
+
+
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,14 +1,85 @@
 package io.zipcoder;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StudentTest {
 
     @Test
     public void testStudentConstructor(){
         //Given
-        double[] scores = new double[67];
+        ArrayList<Double> scores = new ArrayList<>(Arrays.asList(58.9, 96.5, 104.5));
+        Student student = new Student("Tiger", "Woods", scores);
 
+
+        //Then
+        Assert.assertEquals("Tiger", student.getFirstName());
+        Assert.assertEquals("Woods", student.getLastName());
+        Assert.assertEquals(87.0,student.getAverageScore(),.02);
+
+    }
+
+    @Test
+    public void getFirstName() {
+        //Given
+        Student student = new Student("Tiger", "Woods", null);
+
+        //Then
+        Assert.assertEquals("Tiger", student.getFirstName());
+
+    }
+
+    @Test
+    public void setFirstName() {
+        //Given
+        Student student = new Student("Tiger", "Woods", null);
+        student.setFirstName("Charlie");
+
+        //Then
+        Assert.assertEquals("Charlie", student.getFirstName());
+    }
+
+    @Test
+    public void getLastName() {
+        //Given
+        Student student = new Student("Tiger", "Woods", null);
+
+        //Then
+        Assert.assertEquals("Woods", student.getLastName());
+
+
+    }
+
+    @Test
+    public void setLastName() {
+        //Given
+        Student student = new Student("Tiger", "Woods", null);
+        student.setLastName("Smith");
+
+        //Then
+        Assert.assertEquals("Smith", student.getLastName());
+    }
+
+    @Test
+    public void addExamScore() {
+        //Given
+        Student student = new Student("Tiger", "Woods", new ArrayList<>());
+        student.addExamScore(58.96);
+
+        //Then
+        Assert.assertEquals(new ArrayList<>(Arrays.asList(59.0)), student.getExamScore());
+    }
+
+    @Test
+    public void getAverageScore() {
+        //Given
+        Student student = new Student("Tiger", "Woods", new ArrayList<>(Arrays.asList(34.8, 90.4, 99.9)));
+
+        //Then
+        Assert.assertEquals(75.03, student.getAverageScore(), 0.2);
     }
 
 }
